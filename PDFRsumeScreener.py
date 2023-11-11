a27f08cf376d61f86e8a48b7996959d15e757966
Isaac Wan <isaacwan7@gmail.com>
2023-11-11 12:48:37 -0600

    Implemented getBestResume and getPDFJobDesciption functions
    Print statements at the bottom can be edited to work with the given resume directories and a sample job description provided at the top.

--- PDFRsumeScreener.py
+++ PDFRsumeScreener.py
@@ -1,27 +1,83 @@
 import pdftotext
+import os
+import itertools
+import math
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-with open ("NLP Resume Files/data/INFORMATION-TECHNOLOGY/10089434.pdf", "rb") as f:
-    pdf = pdftotext.PDF(f)
+directoryAccountant = 'NLP Resume Files/data/ACCOUNTANT/'
+directoryAdvocate = 'NLP Resume Files/data/ADVOCATE/'
+directoryAgriculture = 'NLP Resume Files/data/AGRICULTURE/'
+directoryApparel = 'NLP Resume Files/data/APPAREL/'
+directoryArts = 'NLP Resume Files/data/ARTS/'
+directoryAutomobile = 'NLP Resume Files/data/AUTOMOBILE/'
+directoryAviation = 'NLP Resume Files/data/AVIATION/'
+directoryBanking = 'NLP Resume Files/data/BANKING/'
+directoryBpo = 'NLP Resume Files/data/BPO/'
+directoryBusiness = 'NLP Resume Files/data/BUSINESS-DEVELOPMENT/'
+directoryChef = 'NLP Resume Files/data/CHEF/'
+directoryConstruction = 'NLP Resume Files/CONSTRUCTION/'
+directoryConsultant = 'NLP Resume Files/data/CONSULTANT/'
+directoryDesigner = 'NLP Resume Files/data/DESIGNER/'
+directoryDigitalMedia = 'NLP Resume Files/data/DIGITAL-MEDIA/'
+directoryEngineering = 'NLP Resume Files/data/ENGINEERING/'
+directoryFinance = 'NLP Resume Files/data/FINANCE/'
+directoryFitness = 'NLP Resume Files/data/FITNESS/'
+directoryHealthcare = 'NLP Resume Files/data/HEALTHCARE/'
+directoryHR = 'NLP Resume Files/data/HR/'
+directoryInfoTech = 'NLP Resume Files/data/INFORMATION-TECHNOLOGY/'
+directoryPR = 'NLP Resume Files/data/PUBLIC-RELATIONS/'
+directorySales = 'NLP Resume Files/data/SALES/'
+directoryTeacher = 'NLP Resume Files/data/TEACHER/'
 
-print(pdf[0])
+sampleJob = "NLP Resume Files/Jobs/Sample Job Description.pdf"
+def getPDFJobDescription(jobPath):
+    with open (jobPath, "rb") as j:
+        jobText = pdftotext.PDF(j)
+        completejobDesciption = "\n\n".join(jobText)
+    return completejobDesciption
 
-with open ("NLP Resume Files/Jobs/Sample Job Description.pdf", "rb") as j:
-    job = pdftotext.PDF(j)
+def getBestResume(resumeDirectory, jobText):
+    resumeScoreHolder = dict()
+    for pdfFile in os.listdir(resumeDirectory):
+        completeResume = os.path.join(resumeDirectory, pdfFile)
+        if os.path.isfile(completeResume):
+            with open (completeResume, "rb") as f:
+                pdf = pdftotext.PDF(f)
+                resumeText = "\n\n".join(pdf)
+                content = [jobText, resumeText]
+                cv = CountVectorizer()
+                matrix = cv.fit_transform(content)
+                similarity_matrix = cosine_similarity(matrix)
+                print('------ resume  ------')
+                result = str(similarity_matrix[1][0]*100)
+                print('Current Resume:' + pdfFile)
+                print('Resume matches by:'+ result + '%\n')
+                resumeScoreHolder.update({completeResume : result})
+    bestResume = max(resumeScoreHolder, key = resumeScoreHolder.get)
+    bestResumeScore = resumeScoreHolder[bestResume]
+    return bestResume, bestResumeScore
 
-print(job[0])
-# resume = docx2txt.process('content/Keller_Tech_Resume9.18.23.docx')
+# print(getBestResume(directoryInfoTech, getPDFJobDescription(sampleJob)))
+print(getBestResume(directoryPR, getPDFJobDescription(sampleJob)))
 
-# print(resume)
 
-# content = [job_description, resume]
+# for pdfFile in os.listdir(directoryInfoTech):
+#     infoTechFiles = os.path.join(directoryInfoTech, pdfFile)
+#     if os.path.isfile(infoTechFiles):
+#         with open (infoTechFiles, "rb") as f:
+#             pdf = pdftotext.PDF(f)
+#             resumeText = "\n\n".join(pdf)
+#             content = [jobText, resumeText]
+#             cv = CountVectorizer()
+#             matrix = cv.fit_transform(content)
+#             similarity_matrix = cosine_similarity(matrix)
+#             print('------ resume  ------')
+#             result = str(similarity_matrix[1][0]*100)
+#             print('Current Resume:' + pdfFile)
+#             print('Resume matches by:'+ result + '%\n')
+#             scoreHolder.update({infoTechFiles : result})
 
-# from sklearn.feature_extraction.text import CountVectorizer
-# cv = CountVectorizer()
-# matrix = cv.fit_transform(content)
-
-# from sklearn.metrics.pairwise import cosine_similarity
-# similarity_matrix = cosine_similarity(matrix)
-
-# print(similarity_matrix)
-
-# print('Resume matches by: '+ str(similarity_matrix[1][0]*100)+ '%')
+# maxResume = max(scoreHolder, key = scoreHolder.get)
+# print('The best resume: ' + maxResume)
+# print('Its score: ' + scoreHolder[maxResume])
