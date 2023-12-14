3354fcf1a305d0988a7999b8b1a4fc808a526d8b
Isaac Wan <isaacwan7@gmail.com>
2023-12-14 15:53:30 -0600

    Update scklearn.py

--- scklearn.py
+++ scklearn.py
@@ -1,7 +1,5 @@
 import pdftotext
 import os
-import itertools
-import math
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -31,6 +29,15 @@ directorySales = 'NLP Resume Files/data/SALES/'
 directoryTeacher = 'NLP Resume Files/data/TEACHER/'
 directoryStudentResumes = 'NLP Resume Files/Student Resumes/'
 directoryCleanResumes = 'NLP Resume Files/Cleaned Resumes/'
+
+resume = "NLP Resume Files/Fullstack-Developer-Resume.txt"
+job_description = "NLP Resume Files/Jobs/Job-Description.txt"
+
+sampleResume = "/Users/jackkeller/Desktop/484F23/project-jack_isaac/NLP Resume Files/Cleaned Resumes/Edited JessicaMcAllister.txt"
+testDirectory = '/Users/jackkeller/Desktop/484F23/project-jack_isaac/NLP Resume Files/Student Resumes'
+
+job_description = "**Job Title: Entry-Level Software Engineer* [Company Name] is a dynamic and innovative software development company committed to creating cutting-edge solutions that redefine industries. We pride ourselves on fostering a collaborative and inclusive work environment where creativity and technical excellence thrive. As we continue to expand our team, we are seeking passionate and talented individuals to join us on our journey. **Position Overview:** We are looking for an entry-level software engineer to contribute to the design, development, and maintenance of our software applications. As a key member of our engineering team, you will work closely with experienced developers, participating in various stages of the software development lifecycle. This is an excellent opportunity for a recent graduate or an individual with limited professional experience to grow their skills in a supportive and challenging environment.**Responsibilities:** Collaborate with cross-functional teams to understand project requirements and specifications. Assist in the design and implementation of software solutions, following best practices and coding standards. Write clean, efficient, and well-documented code. Participate in code reviews to ensure the quality of the codebase. Debug and troubleshoot software defects and issues. Learn and adapt to new technologies and programming languages as needed. Contribute ideas to improve existing processes and workflows. **Qualifications:** Bachelor’s degree in Computer Science, Software Engineering, or a related field. Strong understanding of programming fundamentals and object-oriented design. Proficiency in at least one programming language (e.g., Java, C++, Python, etc.). Knowledge of software development tools and methodologies. Excellent problem-solving and analytical skills. Good communication and teamwork abilities. Demonstrated passion for software development through personal projects, internships, or coursework.**Preferred Skills:**  Familiarity with web development technologies (HTML, CSS, JavaScript). Experience with version control systems (Git, SVN). Understanding of database concepts and SQL. 4. Knowledge of software testing principles. 5. Exposure to agile development methodologies. **Perks and Benefits:** 1. Competitive salary and performance-based bonuses. 2. Comprehensive health, dental, and vision insurance. 3. Flexible work hours and remote work options. 4. Opportunities for professional development and career growth. 5. Fun and collaborative work environment with team-building activities."
+
 
 list = ["the", "a", "about", "above", "actually", "after", "again", "against", "all", "almost", "also", "although", "always", "am", "an", "and", "any", "are", "as", "at",
         "be", "became", "become", "because", "bin", "before", "being", "below", "between", "both", "but", "by", 
@@ -63,8 +70,9 @@ def getBestResume(resumeDirectory, jobText):
     resumeScoreHolder = dict()
     for file in os.listdir(resumeDirectory):
         completeResume = os.path.join(resumeDirectory, file)
+        print(file)
         if os.path.isfile(completeResume):
-            with open (completeResume, "r") as f:
+            with open (completeResume, "rb") as f:
                 pdf = pdftotext.PDF(f)
                 resumeText = "\n\n".join(pdf)
                 content = [jobText, resumeText]
@@ -73,32 +81,29 @@ def getBestResume(resumeDirectory, jobText):
                 similarity_matrix = cosine_similarity(matrix)
                 print('------ new resume  ------')
                 result = str(similarity_matrix[1][0]*100)
-                if 
                 resumeScoreHolder.update({completeResume : result})
     bestResume = max(resumeScoreHolder, key = resumeScoreHolder.get)
     bestResumeScore = resumeScoreHolder[bestResume]
     return bestResume, bestResumeScore, counter
 
-print(getBestResume(directoryInfoTech, getPDFJobDescription(job_description)))
-# print(getBestResume(directoryChef, getPDFJobDescription(sampleJob)))
+print(getBestResume(directoryInfoTech, job_description))
+
+print(getBestResume(directoryBanking, job_description))
+
+print(getBestResume(directoryChef, job_description))
 
 # print(getBestResume(directoryCleanResumes, sampleJob))
 
-resume = "NLP Resume Files/Fullstack-Developer-Resume.txt"
-job_description = "NLP Resume Files/Jobs/Job-Description.txt"
 
-sampleResume = "/Users/jackkeller/Desktop/484F23/project-jack_isaac/NLP Resume Files/Cleaned Resumes/Edited JessicaMcAllister.txt"
-testDirectory = '/Users/jackkeller/Desktop/484F23/project-jack_isaac/NLP Resume Files/Student Resumes'
-job_description = "**Job Title: Entry-Level Software Engineer* [Company Name] is a dynamic and innovative software development company committed to creating cutting-edge solutions that redefine industries. We pride ourselves on fostering a collaborative and inclusive work environment where creativity and technical excellence thrive. As we continue to expand our team, we are seeking passionate and talented individuals to join us on our journey. **Position Overview:** We are looking for an entry-level software engineer to contribute to the design, development, and maintenance of our software applications. As a key member of our engineering team, you will work closely with experienced developers, participating in various stages of the software development lifecycle. This is an excellent opportunity for a recent graduate or an individual with limited professional experience to grow their skills in a supportive and challenging environment.**Responsibilities:** Collaborate with cross-functional teams to understand project requirements and specifications. Assist in the design and implementation of software solutions, following best practices and coding standards. Write clean, efficient, and well-documented code. Participate in code reviews to ensure the quality of the codebase. Debug and troubleshoot software defects and issues. Learn and adapt to new technologies and programming languages as needed. Contribute ideas to improve existing processes and workflows. **Qualifications:** Bachelor’s degree in Computer Science, Software Engineering, or a related field. Strong understanding of programming fundamentals and object-oriented design. Proficiency in at least one programming language (e.g., Java, C++, Python, etc.). Knowledge of software development tools and methodologies. Excellent problem-solving and analytical skills. Good communication and teamwork abilities. Demonstrated passion for software development through personal projects, internships, or coursework.**Preferred Skills:**  Familiarity with web development technologies (HTML, CSS, JavaScript). Experience with version control systems (Git, SVN). Understanding of database concepts and SQL. 4. Knowledge of software testing principles. 5. Exposure to agile development methodologies. **Perks and Benefits:** 1. Competitive salary and performance-based bonuses. 2. Comprehensive health, dental, and vision insurance. 3. Flexible work hours and remote work options. 4. Opportunities for professional development and career growth. 5. Fun and collaborative work environment with team-building activities."
 
-content = [sampleJob, job_description]
-cv = CountVectorizer(stop_words=None)
-matrix = cv.fit_transform(content)
-similarity_matrix = cosine_similarity(matrix)
-print('------ new resume  ------')
-result = str(similarity_matrix[1][0]*100)
-# print('Current Resume:' + pdfFile)
-print('Resume matches by:'+ result + '%\n')
+# content = [sampleJob, job_description]
+# cv = CountVectorizer(stop_words=None)
+# matrix = cv.fit_transform(content)
+# similarity_matrix = cosine_similarity(matrix)
+# print('------ new resume  ------')
+# result = str(similarity_matrix[1][0]*100)
+# # print('Current Resume:' + pdfFile)
+# print('Resume matches by:'+ result + '%\n')
 
 
 # for pdfFile in os.listdir(directoryInfoTech):
